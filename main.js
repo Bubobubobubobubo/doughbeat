4436bbcf456bc973a7677e3fa33ac8336a6d16e5
Raphael Forment <raphael.forment@gmail.com>
2023-12-10 16:01:39 +0100

    Limit evaluation time

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ import { getSimpleDynamicWorklet } from "./helpers.js";
 let ac;
 let crossfadeDuration = 2; // Adjust the crossfade duration as needed
 let workerPile = [];
-
-
+let lastCall = Date.now();
 
 const stop = async () => {
-  // Stop all sounds
   ac = ac || new AudioContext();
-  for (worker in workerPile) {
-    worker.stop();
+  for (const worker of workerPile) {
+    worker.gain.gain.setValueAtTime(1, ac.currentTime);
+    worker.gain.gain.linearRampToValueAtTime(0, ac.currentTime + crossfadeDuration);
+    setTimeout(() => {
+      worker.node.stop();
+      worker.gain.disconnect();
+    }, crossfadeDuration * 1000);
   }
 };
 
@@ -28,17 +31,26 @@ const addNewSynth = async (code) => {
 
 const update = async (code) => {
   ac = ac || new AudioContext();
-  console.log(workerPile);
+  // Prevents rapid-fire calls to update
+  if (lastCall + crossfadeDuration * 1000 > Date.now()) {
+    console.log("Too many calls to update!")
+    return;
+  }
+  lastCall = Date.now();
+
+  console.log("Evaluting code: {}", code);
+
+  // Maximum of two synths at a time
   if (workerPile.length < 2) {
     addNewSynth(code);
   } else {
+    addNewSynth(code);
     let oldest = workerPile.shift();
     oldest.gain.gain.setValueAtTime(1, ac.currentTime);
     oldest.gain.gain.linearRampToValueAtTime(0, ac.currentTime + crossfadeDuration);
     setTimeout(() => {
       oldest.node.stop();
       oldest.gain.disconnect();
-      addNewSynth(code);
     }, crossfadeDuration * 1000);
   }
   window.location.hash = "#" + btoa(code);
@@ -67,6 +79,7 @@ const initialCode =
   return (110 * t % 1 - 0.5) / 10
 }`;
 input.value = initialCode;
+
 input.addEventListener("keydown", (e) => {
   if (e.ctrlKey && e.key === "Enter") {
     update(input.value);
